dd6f8eddbdd6c16236e7d62ecd6c86453cfddc8e
JulianH06 <Julian.hirth@gmail.com>
2026-01-15 02:55:29 +0100

    fixed wood textures, improved the helper and added a prof bomb highlight to it

--- src/main/java/julianh06/wynnextras/mixin/ProfessionHighlightFeatureMixin.java
+++ src/main/java/julianh06/wynnextras/mixin/ProfessionHighlightFeatureMixin.java
@@ -4,6 +4,8 @@ import com.wynntils.core.components.Models;
 import com.wynntils.features.ui.ProfessionHighlightFeature;
 import com.wynntils.mc.event.ScreenInitEvent;
 import com.wynntils.models.containers.containers.CraftingStationContainer;
+import julianh06.wynnextras.config.WynnExtrasConfig;
+import julianh06.wynnextras.config.simpleconfig.SimpleConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,6 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ProfessionHighlightFeatureMixin {
     @Inject(method = "onScreenInit", at = @At(value = "HEAD"), cancellable = true, remap = false)
     void onScreenInit(ScreenInitEvent.Pre event, CallbackInfo ci) {
-        if((Models.Container.getCurrentContainer() instanceof CraftingStationContainer)) ci.cancel();
+        if((Models.Container.getCurrentContainer() instanceof CraftingStationContainer) && SimpleConfig.getInstance(WynnExtrasConfig.class).craftingHelperOverlay) ci.cancel();
     }
 }
